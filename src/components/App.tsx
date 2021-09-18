3cd54fc56208ac6b740df2fbe107d325895090de
kriziu <brunodzi07@gmail.com>
2021-09-18 12:25:28 +0200

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+const App: FC = (): JSX.Element => {
+    return <h1>App</h1>
+}
+
+export default App;
+
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+const path = document.getElementById('root') as HTMLElement;
+
+ReactDOM.render(<App />, path);
